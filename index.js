fc85f2ab1d28e0abfad19ae226b2de574ffd72c8
Brian <Briandoran1@icloud.com>
2021-10-03 18:29:13 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const countDown = (num) => {
+while(num>=0){
+console.log(num)
+num = num-1
+
+
+}
+
+}
+
+countDown(10)
+
+const writeCards = (arr,event) => {
+    let output = []
+for(let i=0;i<arr.length;i++){
+const message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+output.push(message)
+
+}
+return output
+
+}
+writeCards(['bob','joe','Brian'],'Birthday')
